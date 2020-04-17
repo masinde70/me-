56160a628082bde86e346d3e35abc777059f7b48
masinde70 <masinde70@gmail.com>
2020-04-17 11:44:43 +0300

    optional

---
+++ optionals.hpp
@@ -0,0 +1,17 @@
+
+#pragma_once
+#include <optional>
+#include <iostream>
+#include <string>
+
+class UserName {
+public:
+  explicit UserName(std::string str) : mName(std::move(str)){
+    std::cout << "UserName::UserName('" << mName << "')\n";
+  }
+  ~UserName(){
+    std::cout << "UserName::~UserName('" << mName << " ')\n";
+  }
+private:
+  std::string mName;
+};
